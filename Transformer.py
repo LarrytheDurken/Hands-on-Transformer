e52fb994d90269293928f24cfd538a1c8babdde5
ishice <1426064355@qq.com>
2025-02-28 17:11:07 +0800

    [2.28] 完成建立model的方法

--- Transformer.py
+++ Transformer.py
@@ -258,3 +258,24 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
+def make_model(
+        src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1
+):
+    # 注意这里没有mask的事
+    c = copy.deepcopy()
+    attn = MultiHeadedAttention(h, d_model)
+    ff = PositionwiseFeedForward(d_model, dropout)
+    position = PositionalEncoding(d_model, dropout)
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab),
+    )
+
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+
+    return model
